c04bfafe66e9453b22796edea8ca38e68706a7e7
jps-wilson <jwilson316@my.bcit.ca>
2026-03-13 09:43:18 -0700

    starting fresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
 function App() {
-  const year = new Date().getFullYear();
-
   return (
     <>
-      {/* Start header */}
-      <header>
-        <a href='#'>JW</a>
-        <nav>
-          <ul>
-            <li>
-              <a href='#'>Work</a>
-            </li>
-            <li>
-              <a href='#'>About</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      {/* End header */}
-
-      {/* Start Main */}
-      <main>
-        <h1>Welcome</h1>
-      </main>
-      {/* End Main */}
-      <footer>
-        <p>&copy; Jess Wilson {year}</p>
-      </footer>
+      <Home />
     </>
   );
 }
