f88c43ddc64fb041730fafbd82476a74670675a6
jps-wilson <jwilson316@my.bcit.ca>
2026-03-16 20:05:57 -0700

    Add Work page layout and project grid

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,5 +1,31 @@
+import SectionLabel from "../components/ui/SectionLabel";
+import ProjectCard from "../components/ui/ProjectCard";
+import { projects } from "../data/projects";
+import "../styles/pages/work.css";
+
 function Work() {
-  return <div>Work</div>;
+  return (
+    <div className='work'>
+      <div className='work__header'>
+        <SectionLabel text='Work' />
+        <p className='work__subtitle'>Four projects. One belief.</p>
+      </div>
+
+      <div className='work__grid'>
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            number={project.number}
+            title={project.title}
+            idea={project.idea}
+            stack={project.stack}
+            accent={project.accent}
+            path={project.path}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Work;
