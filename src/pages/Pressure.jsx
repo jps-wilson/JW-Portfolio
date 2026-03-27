d4d39b48cf7dd33a0d004f3c4de2c146e7d14443
jps-wilson <jwilson316@my.bcit.ca>
2026-03-27 13:35:45 -0700

    Use ProjectPage embed prop for Pressure
    Pass an embedUrl prop to ProjectPage in Pressure.jsx and remove the separate ProjectEmbed component/import. Simplifies page markup by consolidating embed rendering into ProjectPage and removes an unused import.

--- src/pages/Pressure.jsx
+++ src/pages/Pressure.jsx
@@ -1,15 +1,15 @@
 import ProjectPage from "../components/ui/ProjectPage";
-import ProjectEmbed from "../components/projects/ProjectEmbed";
 import { projects } from "../data/projects";
 import screenshot from "../assets/screenshots/pressure.png";
 
 function Pressure() {
   const project = projects.find((p) => p.id === "pressure");
   return (
-    <>
-      <ProjectPage project={project} screenshot={screenshot} />
-      <ProjectEmbed url='https://pressure-app.vercel.app/' />
-    </>
+    <ProjectPage
+      project={project}
+      screenshot={screenshot}
+      embedUrl='https://pressure-app.vercel.app/'
+    />
   );
 }
 
