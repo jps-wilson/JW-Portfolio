e7b835d1a2ee6421c16be8a10806da98cd60ace8
jps-wilson <jwilson316@my.bcit.ca>
2026-03-28 18:40:02 -0700

    Embed Deadwax case study in project page
    Import and render DeadwaxCaseStudy as a child of ProjectPage for the Deadwax project. The change wraps the existing ProjectPage JSX with <DeadwaxCaseStudy /> so the case study content is displayed alongside the screenshot and project data.

--- src/pages/Deadwax.jsx
+++ src/pages/Deadwax.jsx
@@ -1,10 +1,15 @@
 import ProjectPage from "../components/ui/ProjectPage";
+import DeadwaxCaseStudy from "../components/projects/DeadwaxCaseStudy";
 import { projects } from "../data/projects";
 import screenshot from "../assets/screenshots/deadwax.png";
 
 function Deadwax() {
   const project = projects.find((p) => p.id === "deadwax");
-  return <ProjectPage project={project} screenshot={screenshot} />;
+  return (
+    <ProjectPage project={project} screenshot={screenshot}>
+      <DeadwaxCaseStudy />
+    </ProjectPage>
+  );
 }
 
 export default Deadwax;
