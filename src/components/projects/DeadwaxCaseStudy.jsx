5f31f67382d4a962c0c1bc0472a615000c360af1
jps-wilson <jwilson316@my.bcit.ca>
2026-04-07 21:22:42 -0700

    Refactor case study components to use CodeSnippet for code highlighting; remove redundant code

--- src/components/projects/DeadwaxCaseStudy.jsx
+++ src/components/projects/DeadwaxCaseStudy.jsx
@@ -1,12 +1,5 @@
-import { useEffect, useRef } from "react";
-import hljs from "highlight.js/lib/core";
-import javascript from "highlight.js/lib/languages/javascript";
-import css from "highlight.js/lib/languages/css";
-import "highlight.js/styles/atom-one-dark.min.css";
+import CodeSnippet from "../ui/CodeSnippet";
 import "../../styles/components/case-study.css";
-
-hljs.registerLanguage("javascript", javascript);
-hljs.registerLanguage("css", css);
 
 const sections = [
   {
@@ -79,37 +72,6 @@ const sections = [
     },
   },
 ];
-
-function CodeSnippet({ snippet }) {
-  const codeRef = useRef(null);
-
-  useEffect(() => {
-    if (codeRef.current) {
-      hljs.highlightElement(codeRef.current);
-    }
-  }, [snippet.code]);
-
-  return (
-    <div className='cs-snippet'>
-      <div className='cs-snippet__header'>
-        <span className='cs-snippet__filename'>{snippet.filename}</span>
-        <span className='cs-snippet__lang'>{snippet.language}</span>
-      </div>
-      <pre
-        className='cs-snippet__pre'
-        aria-label={`Code snippet: ${snippet.filename}`}
-      >
-        <code
-          ref={codeRef}
-          className={`language-${snippet.language.toLowerCase()}`}
-          tabIndex='0'
-        >
-          {snippet.code}
-        </code>
-      </pre>
-    </div>
-  );
-}
 
 function DeadwaxCaseStudy() {
   return (
