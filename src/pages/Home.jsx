fa636f7a6c008a4e518d244a32547d358e0d0944
jps-wilson <jwilson316@my.bcit.ca>
2026-05-04 16:22:17 -0700

    Add lazy loading to featured image for improved performance

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -110,6 +110,7 @@ function Home() {
               src={screenshots[projects[0].id]}
               alt={projects[0].title}
               className='home-featured__image'
+              loading='lazy'
             />
           </div>
         </Link>
